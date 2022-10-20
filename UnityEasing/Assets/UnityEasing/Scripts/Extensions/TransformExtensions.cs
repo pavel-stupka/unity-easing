01a11c59a9e709d7fc238314459cf0e21094f95d
Pavel Stupka <pavel.stupka@simopt.cz>
2022-10-20 09:26:08 +0200

    Documentation added to TransformExtensions class.

--- UnityEasing/Assets/UnityEasing/Scripts/Extensions/TransformExtensions.cs
+++ UnityEasing/Assets/UnityEasing/Scripts/Extensions/TransformExtensions.cs
@@ -8,18 +8,41 @@ namespace UnityEasing
     /// </summary>
     public static class TransformExtensions
     {
+        /// <summary>
+        /// Executes a movement with a target position defined by Vector3.
+        /// </summary>
+        /// <param name="transform"></param>
+        /// <param name="target"></param>
+        /// <param name="duration"></param>
+        /// <param name="easingType"></param>
+        /// <param name="delay"></param>
+        /// <param name="space"></param>
         public static void MoveTo(this Transform transform, Vector3 target, float duration = 1.0f, EasingType easingType = EasingType.Linear, float delay = 0.0f, Space space = Space.World)
         {
             var moveToBehaviour = GetMoveToBehaviour(transform);
             moveToBehaviour.MoveTo(target, duration, easingType, delay, space);
         }
-        
+
+        /// <summary>
+        /// Executes a movement with a target position defined by transform position.
+        /// </summary>
+        /// <param name="transform"></param>
+        /// <param name="target"></param>
+        /// <param name="duration"></param>
+        /// <param name="easingType"></param>
+        /// <param name="delay"></param>
+        /// <param name="space"></param>
         public static void MoveTo(this Transform transform, Transform target, float duration = 1.0f, EasingType easingType = EasingType.Linear, float delay = 0.0f, Space space = Space.World)
         {
             var moveToBehaviour = GetMoveToBehaviour(transform);
             moveToBehaviour.MoveTo(target, duration, easingType, delay, space);
         }
 
+        /// <summary>
+        /// Gets (and creates is necessary) the MoveToBehaviour component on target transform.
+        /// </summary>
+        /// <param name="component"></param>
+        /// <returns></returns>
         private static MoveToBehaviour GetMoveToBehaviour(Component component)
         {
             var moveToBehaviour = component.GetComponent<MoveToBehaviour>();
